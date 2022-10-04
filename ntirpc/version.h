a14c373fc210933bd29a4ba3fc0b8955314211d1
Hyma200 <51007919+Hyma200@users.noreply.github.com>
2022-10-04 11:40:29 -0500

    Add ntirpc folder

---
+++ ntirpc/version.h
@@ -0,0 +1,15 @@
+/* version.h file expanded by Cmake for build */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define NTIRPC_VERSION_MAJOR 3
+#define NTIRPC_VERSION_MINOR .5
+#define NTIRPC_VERSION_EXTRA 
+
+#define NTIRPC_VERSION "3.5"
+#define NTIRPC_VERSION_COMMENT "Full-duplex and bi-directional ONC RPC on TCP."
+#define _GIT_HEAD_COMMIT "GITDIR-NOTFOUND"
+#define _GIT_DESCRIBE "GIT-NOTFOUND"
+
+#endif /* VERSION_H */
